ef4af45932641bec3a6c17160d3d750763ed6509
Adam K <adam@kecskes.net>
2020-04-20 17:02:50 -0500

    Got the math right on the angles; was looking at wrong starting value

--- app/views.js
+++ app/views.js
@@ -3,12 +3,18 @@ import { MmmMode } from './MmmTracker';
 let monkArc = () => document.getElementById('monk-arc');
 let monsterArc = () => document.getElementById('monster-arc');
 let marshmallowArc = () => document.getElementById('marshmallow-arc');
+let pauseArc = () => document.getElementById('pause-arc');
 
 export function arcHandler(mmmTracker) {
   const total = mmmTracker.countTotal();
+
   monkArc().sweepAngle = (mmmTracker.getCount(MmmMode.monk) / total) * 360;
+
+  monsterArc().startAngle = monkArc().sweepAngle;
   monsterArc().sweepAngle =
     (mmmTracker.getCount(MmmMode.monster) / total) * 360;
+
+  marshmallowArc().startAngle = monsterArc().sweepAngle + monkArc().sweepAngle;
   marshmallowArc().sweepAngle =
     (mmmTracker.getCount(MmmMode.marshmallow) / total) * 360;
 }
